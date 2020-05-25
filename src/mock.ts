dfcfa4f6a1e49221fbc306b972c3b4ec9c254d4f
fy <fy0@qq.com>
2020-05-26 00:41:17 +0800

    简易API设计

--- src/mock.ts
+++ src/mock.ts
@@ -1,3 +1,93 @@
-import Mock from 'better-mock'
+import Mock, { MockCbOptions } from 'better-mock'
+import CONFIG from './config'
 
-Mock.mock('aa', {})
+let base = `${CONFIG.API_SERVER}/api/v1`
+
+// 获取清单列表
+Mock.mock(`${base}/checklist/list`, {
+  'data|1-10': [
+    {
+      'id': '@guid',
+      'name': '@city',
+      'createTime|1590413694-1590443694': 1590423694,
+      'updateTime|1590413694-1590443694': 1590423694,
+    }
+  ]
+})
+
+// 添加清单
+Mock.mock(`${base}/checklist/add`, 'post', (req: MockCbOptions) => {
+  console.log(req.body)
+  return Mock.mock({
+    'id': '@guid',
+    'name': '@city',
+    'createTime|1590413694-1590443694': 1590423694,
+    'updateTime|1590413694-1590443694': 1590423694,
+  })
+})
+
+// 添加todo
+Mock.mock(`${base}/todo/add`, 'post', (req: MockCbOptions) => {
+  console.log(req.body)
+  return Mock.mock({
+    'id': '@guid',
+    'name': '@city',
+    'createTime|1590413694-1590443694': 1590423694,
+    'updateTime|1590413694-1590443694': 1590423694,
+  })
+})
+
+// 全部
+Mock.mock(`${base}/todo/list/all`, 'post', (req: MockCbOptions) => {
+  console.log(req.body)
+  return Mock.mock({
+    'id': '@guid',
+    'name': '@city',
+    'createTime|1590413694-1590443694': 1590423694,
+    'updateTime|1590413694-1590443694': 1590423694,
+  })
+})
+
+// 全部
+Mock.mock(`${base}/todo/list/by_checklist`, 'post', (req: MockCbOptions) => {
+  console.log(req.body)
+  return Mock.mock({
+    'id': '@guid',
+    'name': '@city',
+    'createTime|1590413694-1590443694': 1590423694,
+    'updateTime|1590413694-1590443694': 1590423694,
+  })
+})
+
+// 今日
+Mock.mock(`${base}/todo/list/mytoday`, 'post', (req: MockCbOptions) => {
+  console.log(req.body)
+  return Mock.mock({
+    'id': '@guid',
+    'name': '@city',
+    'createTime|1590413694-1590443694': 1590423694,
+    'updateTime|1590413694-1590443694': 1590423694,
+  })
+})
+
+// 重要
+Mock.mock(`${base}/todo/list/important`, 'post', (req: MockCbOptions) => {
+  console.log(req.body)
+  return Mock.mock({
+    'id': '@guid',
+    'name': '@city',
+    'createTime|1590413694-1590443694': 1590423694,
+    'updateTime|1590413694-1590443694': 1590423694,
+  })
+})
+
+// 日程
+Mock.mock(`${base}/todo/list/schedule`, 'post', (req: MockCbOptions) => {
+  console.log(req.body)
+  return Mock.mock({
+    'id': '@guid',
+    'name': '@city',
+    'createTime|1590413694-1590443694': 1590423694,
+    'updateTime|1590413694-1590443694': 1590423694,
+  })
+})
--- src/types/todo.ts
+++ src/types/todo.ts
@@ -10,5 +10,9 @@ export interface Todo {
   name: string;
   createTime: number;
   updateTime: number;
+  deadlineTime: number; // 截止时间
+  isDone: boolean; // 是否已完成
+  // interval // 周期
   note: string | null; // 备注
+  parent: string; // 所属checklist
 }
